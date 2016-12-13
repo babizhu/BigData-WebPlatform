09adc1edbba8eefe9d79da564bed1e80a58fc192
liulaoye <liu_kun@msn.com>
2016-12-13 17:02:49 +0800

    完善了BaseModule的代码格式

--- src/main/java/org/bbz/bigdata/webplatform/module/BaseModule.java
+++ src/main/java/org/bbz/bigdata/webplatform/module/BaseModule.java
@@ -7,22 +7,31 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by liulaoye on 16-10-31.
- * 所有Module的父类
+ * 所有Module的父类，集中处理一些通用逻辑
  */
 public class BaseModule{
 
     /**
      * 根据错误id，构建相应的错误提示信息
      *
-     * @param response
-     * @param errId
+     * @param response　 response
+     * @param errId　    错误ｉｄ
      * @return
+     *          json
      */
     protected NutMap buildErrorResponse( HttpServletResponse response, ErrorCode errId ){
 
         return this.buildErrorResponse( response, errId, "" );
     }
 
+    /**
+     * 统一处理错误情况
+     * @param response  response
+     * @param errId     错误ｉｄ
+     * @param args      错误的相关参数
+     * @return
+     *          json
+     */
     protected NutMap buildErrorResponse( HttpServletResponse response, ErrorCode errId, String args ){
         response.setStatus( 500 );
         NutMap re = new NutMap();
@@ -31,7 +40,12 @@ public class BaseModule{
         return re;
     }
 
+    /**
+     * 在没有返回值的情况下，统一返回{"success":true}
+     * @return
+     *          json
+     */
     protected NutMap buildSuccessResponse(){
-        return new NutMap().addv( "success","1" );
+        return new NutMap().addv( "success",true );
     }
 }
